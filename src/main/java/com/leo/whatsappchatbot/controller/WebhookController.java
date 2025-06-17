8a8c0002cba0f8338a737e5bfb4e6b87f3e55cbb
SIDHANT <sidhantsingh2103@gmail.com>
2025-06-17 12:18:25 +0530

    🔧 Added GET mapping for webhook verification

--- src/main/java/com/leo/whatsappchatbot/controller/WebhookController.java
+++ src/main/java/com/leo/whatsappchatbot/controller/WebhookController.java
@@ -3,11 +3,9 @@ package com.leo.whatsappchatbot.controller;
 import com.leo.whatsappchatbot.dto.MessageDTO;
 import com.leo.whatsappchatbot.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/webhook")
@@ -20,4 +18,16 @@ public class WebhookController {
         String reply = messageService.processMessage(messageDTO);
         return ResponseEntity.ok(reply);
     }
+
+    @GetMapping
+    public ResponseEntity<String> verifyWebhook(@RequestParam("hub.mode") String mode,
+                                                @RequestParam("hub.verify_token") String token,
+                                                @RequestParam("hub.challenge") String challenge) {
+        if ("subscribe".equals(mode) && "salvatoken012".equals(token)) {
+            return ResponseEntity.ok(challenge);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Verification failed");
+        }
+    }
+
 }
